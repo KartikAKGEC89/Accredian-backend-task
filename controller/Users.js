ddd3f1d25aacb4225dd476f49320235e81da9005
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-07-14 22:46:33 +0530

    Forget set email just now set :rocket:

--- controller/Users.js
+++ controller/Users.js
@@ -1,11 +1,13 @@
+const nodemailer = require('nodemailer');
 const User = require('../models/referModel.js');
 
- const Register = async(req, res) => {
-    const { name, email, referrename} = req.body;
+const Register = async (req, res) => {
+    const { name, email, referrename } = req.body;
     if (!referrename || !name || !email) {
         return res.status(400).json({ msg: "Fill all the fields" });
     }
     try {
+
         await User.create({
             name: name,
             email: email,
@@ -13,11 +15,43 @@ const User = require('../models/referModel.js');
             referedAt: new Date(),
             updatedAt: new Date()
         });
-        res.json({ msg: "Registration Successful" });
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,  
+                pass: process.env.PASSWORD        
+            }
+        });
+
+        let htmlMsg = `
+            <h2>Hello ${name},</h2>
+            <p>Thank you for choosing us.</p>
+            <p>${referrename} has referred you to our platform.</p>
+            <p>Best regards,</p>
+            <p>Accredian's</p>
+        `;
+
+        let mailOptions = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: 'Successful',
+            html: htmlMsg
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error occurred while sending email:', error);
+            } else {
+                console.log('Email sent:', info.response);
+            }
+        });
+
+        res.json({ msg: "Successful" });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: "Registration Failed" });
+        res.status(500).json({ msg: "Failed" });
     }
-}
+};
 
-module.exports = {Register}
+module.exports = { Register };
