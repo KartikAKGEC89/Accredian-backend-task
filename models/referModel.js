5e6057e680f8f2f3cb004f969483ff5714c27a5b
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-07-14 15:22:54 +0530

    Model and database config :fire:

---
+++ models/referModel.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const db = require('../config/Database');
+ 
+const { DataTypes } = Sequelize;
+ 
+const User = db.define('users', {
+  name: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  referrename: {
+    type: Sequelize.STRING
+  },
+  referedAt: {
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    type: Sequelize.DATE
+  }
+});
+ 
+(async () => {
+    await db.sync();
+})();
+ 
+export default User;
